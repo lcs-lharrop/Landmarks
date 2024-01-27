91443f7d43183ea51b3d35819ca0987eb44165c9
Lexi Harrop <lharrop@lcs.on.ca>
2024-01-27 10:46:06 -0500

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Lexi on 2024-01-27.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
