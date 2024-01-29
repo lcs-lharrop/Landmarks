573d83de8ef4f79e994687b24a45afceb2f0cf52
Lexi Harrop <lharrop@lcs.on.ca>
2024-01-29 09:39:17 -0500

    {     GalleryView() }

---
+++ Landmarks/Views/GalleryItemView.swift
@@ -0,0 +1,18 @@
+//
+//  GalleryItemView.swift
+//  Landmarks
+//
+//  Created by Lexi on 2024-01-29.
+//
+
+import SwiftUI
+
+struct GalleryItemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GalleryItemView()
+}
