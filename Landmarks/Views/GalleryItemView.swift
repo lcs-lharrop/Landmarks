93582e991988a5c551f295dbae0d546c1aa0ab29
Lexi Harrop <lharrop@lcs.on.ca>
2024-01-29 09:39:36 -0500

    Using the new gallery item view inside a scrolling list, temporarily.

--- Landmarks/Views/GalleryItemView.swift
+++ Landmarks/Views/GalleryItemView.swift
@@ -8,11 +8,45 @@
 import SwiftUI
 
 struct GalleryItemView: View {
+    
+    let item: Landmark
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        VStack(spacing: 0) {
+            Image(item.image)
+                .resizable()
+                .scaledToFill()
+                .frame(height: 125)
+                .clipShape(
+                    Circle()
+                )
+                .padding(.bottom, 10)
+
+            Text(item.name)
+                .font(.headline)
+                .multilineTextAlignment(.center)
+
+            Spacer()
+            
+            Image(systemName: item.isRecommended == true ? "hand.thumbsup.fill" : "hand.thumbsdown")
+                .resizable()
+                .scaledToFit()
+                .frame(height: 20)
+                .padding(5)
+            
+        }
+        .padding()
+        
     }
 }
 
 #Preview {
-    GalleryItemView()
+    HStack {
+        GalleryItemView(item: wonderland)
+            .frame(idealHeight: 200, maxHeight: 250)
+        GalleryItemView(item: sudburyNickel)
+            .frame(idealHeight: 200, maxHeight: 250)
+
+    }
 }
--- Landmarks/Views/GalleryView.swift
+++ Landmarks/Views/GalleryView.swift
@@ -8,8 +8,23 @@
 import SwiftUI
 
 struct GalleryView: View {
+        
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        NavigationStack {
+            
+            List(allLandmarks) { currentLandmark in
+                
+                NavigationLink {
+                    DetailView(item: currentLandmark)
+                } label: {
+                    GalleryItemView(item: currentLandmark)
+                }
+                
+            }
+            .navigationTitle("Landmarks")
+                        
+        }
     }
 }
 
